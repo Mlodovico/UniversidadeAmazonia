8f6c3e811a96a0d05c53e062209bb32afa114331
Murilo Lodovico <37813008+Mlodovico@users.noreply.github.com>
2022-11-03 16:01:11 -0300

    connecting screens and action listeners

--- src/View/MainScreen.java
+++ src/View/MainScreen.java
@@ -1,9 +1,30 @@
 package View;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainScreen {
+public class MainScreen extends JFrame {
     private JButton createButton;
     private JButton deleteButton;
     private JButton updateButton;
+    private JPanel mainPanel;
+
+    public MainScreen() {
+        setContentPane(mainPanel);
+        setTitle("Interface Universidade Amazonia");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StudentForm stdForm = new StudentForm();
+                stdForm.getContentPane();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        MainScreen mainScreen = new MainScreen();
+    }
 }
--- src/View/StudentForm.java
+++ src/View/StudentForm.java
@@ -2,7 +2,7 @@ package View;
 
 import javax.swing.*;
 
-public class StudentForm {
+public class StudentForm extends JFrame{
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
@@ -10,4 +10,16 @@ public class StudentForm {
     private JButton createButton;
     private JButton cancelButton;
     private JCheckBox checkBox1;
+    private JPanel studentPanel;
+
+    public StudentForm() {
+        setTitle("Cadastro de Estudante");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        getContentPane().add(studentPanel);
+    }
+
+    public static void main(String[] args) {
+        StudentForm studentForm = new StudentForm();
+    }
 }
